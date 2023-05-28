c1afdca12b5b1250117e47fc66719520014e4bf8
fhusain01 <fhusain@adaptready.com>
2023-05-28 23:58:13 +0530

    Added middleware for auth route

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 
+// Import routes
+const authRoute = require('./routes/auth')
+
+// Route middlewares
+app.use('/api/user/', authRoute);
+
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
-})
+}) 
